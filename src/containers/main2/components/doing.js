5e1efe7c30eb6efe7d12d03db1b57616a1177651
tiavina-mika <ralainirinatiavina@gmail.com>
2019-09-21 12:44:05 +0300

    add text animation

--- src/containers/main2/components/doing.js
+++ src/containers/main2/components/doing.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Motion from '../../../components/motion';
 import Balloon from './doing/balloon';
 import Blob from './doing/blob';
 import BackgroundBalloon from './doing/background-balloon';
+import TextAnimation from './doing/textAnimation';
+import TextLoop from "react-text-loop";
 
 const string = `If I had to define what my passion is in the larger scheme of things, I would define it to be the fact that software is written by people, for people. So things like “customer service” or “team building” are the things that inspire me and drive me to be the best I can be.`
 
@@ -19,15 +22,27 @@ const useStyles = makeStyles({
         justifyContent: 'center',
         alignItems: 'flex-end'
     },
+    titleContainer: {
+        position: 'absolute',
+        top: '155%',
+        textJustify: 'center',
+        zIndex: 30,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-end'
+    },
     title: {
         fontFamily: 'SofiaPro-Bold',
         color: '#fff',
         fontSize: 135,
-        position: 'absolute',
-        top: '155%',
-        zIndex: 30
+        // width: window.innerWidth - 250
+    },
+    activeTitle: {
+        color: '#01cfcf'
+    },
+    textLoop: {
+        width: 700
     }
-
 })
 
 const Doing = () => {
@@ -41,20 +56,22 @@ const Doing = () => {
         >
             {/* {string} */}
             <div className={classes.blobContainer}>
-                <BackgroundBalloon />
-
                 <Blob
                     size={650}
                     style={{ boxShadow: 'inset 0px 0px 40px rgba(0,0,0,.2), 0 0 90px rgba(0,0,0,.8)', zIndex:5, top: '130%' }} 
                     content={
+                        <>
                         <Blob
                             size={200}
                             style={{ zIndex:10, backgroundColor: '#01cfcf' }}
                             />
+                            {/* <BackgroundBalloon /> */}
+                        </>
                     }/>
-                <Typography variant="h1" className={classes.title}>CE QUE JE PEUX FAIRE</Typography>
+                <TextAnimation /> 
             </div>
 
+            {/* <TextAnimation /> */}
           
         </Motion>
     )
--- src/containers/main2/components/doing/background-balloon.js
+++ src/containers/main2/components/doing/background-balloon.js
@@ -1,39 +1,164 @@
-import React from 'react'
-import Motion from '../../../../components/motionYoyo';
-import Blob from './blob';
+import React, { useState } from 'react';
 
-function getRandomIntInclusive(max) {
-    return Math.floor(Math.random() * Math.floor(max))
+class Item extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: {
+        transform: `translate(${this.props.from[0]}px, ${
+          this.props.from[1]
+        }px) rotate(${Math.random() * 360}deg)`
+      }
+    };
+    this.setStyle = this.setStyle.bind(this);
   }
-const BackgroundBlob = () => {
-  return (
-    [0, 1, 2 , 3, 4, 5].map(n => (
-        // <Motion
-        //     style={{
-        //         background: `url(${process.env.PUBLIC_URL}/images/bubble-blue-${n}.png) right top`,
-        //         backgroundSize: 'cover',
-        //         backgroundRepeat: 'no-repeat',
-        //         position: 'relative',
-        //         height: 50,
-        //         width: 50,
-        //         // top: getRandomIntInclusive(window.innerHeight),
-        //         // left: getRandomIntInclusive(window.innerWidth),
-        //     }}
-        //     animate={{ marginTop: getRandomIntInclusive(10 * n) }}
-        //     duration={2}
-        //  />
-         <Blob
-            size={75}
-            style={{ 
-                boxShadow: 'inset 0px 0px 40px rgba(0,0,0,.2), 0 0 90px rgba(0,0,0,.8)', 
-                zIndex:20, 
-                // top: `${getRandomIntInclusive(20 * (n + 1))}%`, 
-                backgroundColor: '#01cfcf'
-            }} 
-        />
-    ))
-  )
-  
+
+  componentDidMount() {
+    setTimeout(this.setStyle, 1);
+  }
+
+  setStyle() {
+    const movingStyle = `translate(${Math.random() *
+      window.innerHeight *
+      2}px, ${Math.random() * window.innerWidth * 2}px) rotate(${Math.random() *
+      360}deg)`;
+    const time = this.props.time ? this.props.time : 20;
+    const size = this.props.size ? this.props.size : "150px";
+    const style = {
+      position: "absolute",
+      zIndex: "-1",
+      transform: movingStyle,
+      transition: `transform ${time}s ease-out`,
+      MsTransform: movingStyle,
+      MsTransition: `transform ${time}s ease-out`,
+      Webkitransform: movingStyle,
+      WebkitTransition: `transform ${time}s ease-out`,
+      overflow: "visible",
+      willChange: "transform"
+    };
+    const imgSize = {
+      width: size,
+      height: "auto"
+    };
+    this.setState({
+      style: style,
+      size: imgSize
+    });
+    setTimeout(this.setStyle, time * 1000);
+  }
+
+  render() {
+    const { style, size } = this.state;
+
+    return (
+      <div style={style} >
+        <img style={size} srcSet={this.props.what} alt="img" />
+      </ div>
+    );
+  }
 }
 
-export default BackgroundBlob;
+class Sky extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      moves: []
+    };
+    this.movement = this.movement.bind(this);
+  }
+
+  componentDidMount() {
+    const many = this.props.how;
+    const temp_moves = [];
+    for (let i = 0; i < many; i++) {
+      temp_moves.push(this.movement());
+    }
+    this.setState({
+      moves: temp_moves
+    });
+  }
+
+  movement() {
+    const rotation = Math.floor((Math.round(Math.random()) * 2 - 1) * 600);
+    const fromX = Math.floor(Math.random() * window.innerWidth);
+    const fromY = Math.floor(Math.random() * window.innerHeight * 1.5);
+    const toX = Math.floor(
+      Math.random() * window.innerWidth * (Math.round(Math.random()) * 2 - 1)
+    );
+    const toY = Math.floor(
+      Math.random() *
+        window.innerHeight *
+        1.5 *
+        (Math.round(Math.random()) * 2 - 1)
+    );
+    const temp = {
+      rotation,
+      fromX,
+      fromY,
+      toX,
+      toY
+    };
+    return temp;
+  }
+
+  render() {
+    const items = this.props.images;
+    const outerStyle = {
+      position: "absolute",
+      top: "0",
+      left: "0",
+      width: "100%",
+      height: "100%",
+      margin: "0",
+      padding: "0",
+      overflow: "hidden",
+      zIndex: "-1",
+      background: this.props.background ? this.props.background : ""
+    };
+
+    return (
+      <div style={outerStyle} id="sky">
+        {this.state.moves.map((e, i) => {
+          const conditional = Math.floor(
+            Math.random() * Object.keys(items).length
+          );
+
+          return (
+            <Item
+              what={items[conditional]}
+              from={[e.fromX, e.fromY]}
+              to={[e.toX, e.toY]}
+              rotation={e.rotation}
+              size={this.props.size}
+              time={this.props.time}
+              key={i}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+const BackgroundBalloon = () => {
+    const images = {
+        0: `${process.env.PUBLIC_URL}/images/bubble-blue-0.png`,
+        1: `${process.env.PUBLIC_URL}/images/bubble-blue-1.png`,
+        2: `${process.env.PUBLIC_URL}/images/bubble-blue-2.png`,
+        3: `${process.env.PUBLIC_URL}/images/bubble-blue-3.png`,
+        4: `${process.env.PUBLIC_URL}/images/bubble-blue-4.png`,
+        5: `${process.env.PUBLIC_URL}/images/bubble-blue-5.png`,
+    };
+    return (
+      <div style={{ zIndex: 20000}}>
+        <Sky
+          images={images}
+          how={100}
+          size={"100px"}
+          time={30}
+        />
+      </div>
+    );
+}
+
+export default BackgroundBalloon;
